10853dda9c2e598085713db3da72cb1dbd1f3e59
AYUSH VERMA <vayush967@gmail.com>
2018-06-09 23:04:44 +0530

    changes

--- W2/NEXT GREATER ELEMENT O(N^2)/NEXT GREATER ELEMENT O(N^2).cpp
+++ W2/NEXT GREATER ELEMENT O(N^2)/NEXT GREATER ELEMENT O(N^2).cpp
@@ -1,3 +1,4 @@
+// https://www.geeksforgeeks.org/next-greater-element/
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -30,29 +31,29 @@ int nextGreater(int a[],int n){
     long long int sum=0;
     for (int i = 0; i < v.size(); i++)
     {
-        // cout<<v[i]<<" ";
+        cout<<v[i]<<" ";
         sum = sum+v[i];
     }
     return sum;
  }
 
 int main(){
-    // int a[] = {112,133,161,311,122,512,1212,0,19212};
-    // int n = sizeof(a)/sizeof(a[0]);
-    // cout<<nextGreater(a,n)<<endl;
+    int a[] = {8467,6334,6500,9169,5724,1478,9358,6962,4464,5705,8145,3281,6827,9961,491,2995,1942,4827,5436,2391,4604,3902,153,292,2382,7421,8716,9718,9895,5447,1726,4771,1538,1869,9912,5667,6299,7035,9894,8703,3811};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<nextGreater(a,n)<<endl;
 
 
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin>>a[i];
-        }
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     int n;
+    //     cin>>n;
+    //     int a[n];
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         cin>>a[i];
+    //     }
 
-        cout<<nextGreater(a,n)<<endl;
-    }
+    //     cout<<nextGreater(a,n)<<endl;
+    // }
 }
---
+++ W2/NEXT GREATER ELEMENT O(n) imp/NEXT GREATER ELEMENT O(n).cpp
@@ -0,0 +1,50 @@
+// https://practice.geeksforgeeks.org/problems/save-gotham/0
+#include <iostream>
+#include <stack>
+using namespace std;
+int nextGreater(int a[],int n){
+    stack<int> s;
+    s.push(a[0]);
+    long long int sum=0;
+    int i;
+    for(i=1;i<n;i++){
+
+        if(a[i]>s.top()){
+            while(!s.empty() && a[i]>s.top()){
+                sum=sum+a[i];
+                cout<<a[i]<<" ";
+                s.pop();
+            } 
+            s.push(a[i]);
+        }
+        else if(a[i]<s.top()){
+            s.push(a[i]);
+        }
+        if(i==n-1){
+            cout<<"0 ";
+        }
+    }
+
+    return sum;
+}
+int main(){
+    int a[] = {112,133,161,311,122,512,1212,0,19212};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout<<nextGreater(a,n)<<endl;
+
+
+    // int t;
+    // cin>>t;
+    // while(t--){
+    //     int n;
+    //     cin>>n;
+    //     int a[n];
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         cin>>a[i];
+    //     }
+
+    //     cout<<nextGreater(a,n)<<endl;;
+    // }
+    return 0;
+}
