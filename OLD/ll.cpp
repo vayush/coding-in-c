8d03dce1f3df347acbb18953c75a57e2dd38d5ff
AYUSH VERMA <vayush967@gmail.com>
2018-07-27 23:26:11 +0530

    27 JULY

--- OLD/ll.cpp
+++ OLD/ll.cpp
@@ -5,49 +5,38 @@ struct Node
 	int data;
 	Node* next;
 };
-Node *head,*tail;
-void insertAtBeg(int item){
+Node *head;
+void insertatBeg(int item){
 	Node *a;
 	a=new Node();
 	a->data = item;
 	a->next = NULL;
 	if(head == NULL){
 		head = a;
-		tail = a;
-		cout<<"FIRST NODE INSERTED  \n";
+		
 	}
 	else{
 		a->next = head;
 		head = a;
-		cout<<"NODE INSERTED \n";
-	}
+			}
 }
-void display(){
+void Print(){
 	Node *a;
-	if(head==NULL){
-		cout<<"NOTHING TO DISPLAY : ";
-	}
-	else{
-		a = head;
+	a = head;
 		while(a!= NULL){
 			cout<<"-->"<<a->data;
 			a = a->next;
-	}
+	
 		cout<<"\n";
 	}
 }
 int main(){
 	int item,n,i;
 	head = NULL;
-	tail = NULL;
-	cout<<"ENTER HOW MANY NODES TO INSERT : ";
-	cin>>n;
-	cout<<"\n";
-	for(i=0;i<n;i++){
-		cout<<"ENTER ITEM :";
-		cin>>item;
-		insertAtBeg(item);
-		display();
-		}
+	
+	insertatBeg(6);
+	insertatBeg(7);
+	insertatBeg(9);
+	Print();
 		return 0;
 }
